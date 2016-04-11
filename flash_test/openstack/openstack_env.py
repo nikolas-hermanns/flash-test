f463765af203fa1a2c03e19bdc6f5069a9ec40d8
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-04-11 18:34:09 +0200

    Make node load more generic

--- flash_test/openstack/openstack_env.py
+++ flash_test/openstack/openstack_env.py
@@ -35,9 +35,14 @@ class OpenstackEnv(object):
         else:
             raise Exception("Config: %s is not found."
                             % FLASH_TEST_CONFIG_YAML)
-        cls.main_controller = Node(
-             'main_controller',
-             dic=cls.config['env']['nodes']['main_controller'])
+        # add all nodes
+        for (name, node) in cls.config['env']['nodes'].iteritems():
+            new_node = Node(name, node)
+            cls.env_nodes.append(new_node)
+            if name == "main_controller":
+                cls.main_controller = new_node
+
+        # All openstack hypervisors
         nova_hypervisors = cls._novacl.hypervisors.list()
         for nova_hypervisor in nova_hypervisors:
             new_hypervisor = Node(
@@ -48,7 +53,6 @@ class OpenstackEnv(object):
             cls.hypervisors[nova_hypervisor.hypervisor_hostname] =\
                 new_hypervisor
             cls.env_nodes.append(new_hypervisor)
-        cls.env_nodes.append(cls.main_controller)
 
     @classmethod
     def gen_ssh_config(cls, node):
